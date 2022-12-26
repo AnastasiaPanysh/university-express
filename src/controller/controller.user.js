ae448cdc1faac0d221418e0d98da5c8cd7debbd1
AnastasiaPanysh <nastya.panysh@gmail.com>
2022-12-26 23:29:04 +0300

    add new

--- src/controller/controller.user.js
+++ src/controller/controller.user.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { getUsers, getUsersById } = require('../services/services.user')
+const { getUsers, getUsersById, createUsers, updateUsers, deleteUsers, patchUsers } = require('../services/services.user')
 
 const route = express.Router()
 
@@ -16,6 +16,47 @@ route.get('/:id', async (req, res) => {
     try {
         const { id } = req.params
         const user = await getUsersById(id)
+        res.status(200).send(user)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
+
+route.post('/', async (req, res) => {
+    try {
+        const { name, surname, birth, city, age } = req.body
+        const user = await createUsers(name, surname, birth, city, age)
+        res.status(200).send(user)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
+
+route.put('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const { name, surname, birth, city, age } = req.body
+        const user = await updateUsers(id, name, surname, birth, city, age)
+        res.status(200).send(user)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
+
+route.delete('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const user = await deleteUsers(id)
+        res.status(200).send(user)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
+
+route.patch('/:info_id', async (req, res) => {
+    try {
+        const { info_id } = req.params
+        const user = await patchUsers(info_id, req.body)
         res.status(200).send(user)
     } catch (error) {
         res.status(500).send(error.message)
--- src/services/services.user.js
+++ src/services/services.user.js
@@ -1,15 +1,40 @@
-const { getUsersDB, getUsersByIdDB } = require('../repository/repository.user')
+const { getUsersDB, getUsersByIdDB, createUsersDB, updateUsersDB, deleteUsersDB, patchUsersDB } = require('../repository/repository.user')
 
 async function getUsers() {
     const users = await getUsersDB()
     if (!users.length) throw new Error('DB is empty')
     return users
 }
+
 async function getUsersById(id) {
     const users = await getUsersByIdDB(id)
     if (!users.length) throw new Error('DB is empty')
     return users
 }
 
+async function createUsers(name, surname, birth, city, age) {
+    const users = await createUsersDB(name, surname, birth, city, age)
+    if (!users.length) throw new Error('DB is empty')
+    return users
+}
 
-module.exports = { getUsers, getUsersById }
+async function updateUsers(id, name, surname, birth, city, age) {
+    const users = await updateUsersDB(id, name, surname, birth, city, age)
+    if (!users.length) throw new Error('DB is empty')
+    return users
+}
+
+async function deleteUsers(id) {
+    const users = await deleteUsersDB(id)
+    if (!users.length) throw new Error('DB is empty')
+    return users
+}
+
+async function patchUsers(info_id, dataFromClient) {
+    const users = await patchUsersDB(info_id, dataFromClient)
+    if (!users.length) throw new Error('DB is empty')
+    return users
+}
+
+
+module.exports = { getUsers, getUsersById, createUsers, updateUsers, deleteUsers, patchUsers }
